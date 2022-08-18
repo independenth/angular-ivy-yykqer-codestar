30a155a301dbe1c79ac133474d38cbe7ca31989a
independenth <independentz_nutzty@hotmail.com>
2022-08-18 18:15:35 +0700

    1 file modified

--- src/app/order-panel.ts
+++ src/app/order-panel.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
               <br>
               <button (click)="next()">Go</button> 
               <h3>{{current}}
+              <br>
+              <button (click)="toggle()">{{state}}</button>
+              <br>
+              <button (click)="love()">Love</button>
+              <button (click)="hate()">Hate</button>
+              <p *ngFor="let i of data">{{i}}</p>
+              <br>
+              <button (click)="refresh()">Refresh</button>
+              <pre>{{result}}</pre>
+              <br>
+              <input #task>
+              <button (click)="add(task)">Add</button>
+              <p *ngFor="let e of list">{{e}}</p>
             `,
 })
 export class OrderPanel {
@@ -17,5 +30,29 @@ export class OrderPanel {
   company = 'iCoffee';
   show() {
     alert("I Love You")
+  }
+  state = "Yes"
+  toggle() {
+    var old = this.state
+    if (old == "Yes") { this.state = "No"}
+    if (old == "No") { this.state = "Yes"}
+  }
+  data = ["START"]
+  love() {
+    this.data.push("Love")
+  }
+  hate() {
+    this.data.push("Hate")
+  }
+  result = "..."
+  async refresh() {
+    let url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+    let response = await fetch(url)
+    this.result = await response.text()
+  }
+  list = [ ]
+  add(input : any) {
+    this.list.push(input.value)
+    input.value = ""
   }
 }
